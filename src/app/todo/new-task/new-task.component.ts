0c13deffcf2b2ec1b6f4f2952751cb24cb021094
Ruan <rugcosta1234@gmail.com>
2025-06-11 10:03:07 -0300

    fix: removido controle desnecessário com count que bloqueava múltiplas criações de tarefas

--- src/app/todo/new-task/new-task.component.ts
+++ src/app/todo/new-task/new-task.component.ts
@@ -5,24 +5,21 @@ import { TodoService } from 'src/app/shared/services/todo.service';
 @Component({
   selector: 'app-new-task',
   templateUrl: './new-task.component.html',
-  styleUrls: ['./new-task.component.css']
+  styleUrls: ['./new-task.component.css'],
 })
 export class NewTaskComponent {
   newTaskTitle: string = '';
 
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService: TodoService) {}
 
-  count = 0;
   addTask() {
-    if(this.count > 0) return
     const newTodo: Todo = {
       id: this.todoService.getTodoNewId(),
       title: this.newTaskTitle,
-      completed: false
+      completed: false,
     };
 
     this.todoService.addTodo(newTodo);
     this.newTaskTitle = '';
-    this.count++
   }
 }
